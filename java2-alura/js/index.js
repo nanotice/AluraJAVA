8d06567ba6f6c35dc70a39c15238187c6f7f0a3b
Luana Araújo <aninhaluua@gmail.com>
2025-09-24 09:06:30 -0300

    Update index.js

--- java2-alura/js/index.js
+++ java2-alura/js/index.js
@@ -2,8 +2,32 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro-paciente")
-var tdPeso = paciente.querySelector(".info-peso");
+var paciente = document.querySelector("#primeiro-paciente");
 
-console.log(paciente); // tr
-console.log(tdPeso); // td que tem peso
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso < 0 || peso > 1000){
+  console.log("Peso inválido");
+  pesoEhValido = false;
+  tdImc.textContent = "Peso inválido"
+}
+
+if(altura < 0 || altura > 3.00){
+  console.log("Altura inválida");
+  alturaEhValida = false;
+  tdImc.textContent = "Altura inválida"
+}
+
+if (alturaEhValida && pesoEhValido){
+var imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100 / 4.0 =>>>>> 25
+tdImc.textContent = imc;
+}
